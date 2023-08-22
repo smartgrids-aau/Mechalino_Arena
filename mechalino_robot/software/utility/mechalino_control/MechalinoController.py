dacdb54c229614d33a3306f5571a229e84289ff5
k1youssefi@edu.aau.at <SlayerTheGreat10003>
2023-08-22 12:50:24 +0200

    utility controller updated

--- mechalino_robot/software/utility/mechalino_control/MechalinoController.py
+++ mechalino_robot/software/utility/mechalino_control/MechalinoController.py
@@ -3,18 +3,28 @@ from MechalinoControl import MechalinoControl
 
 mechalino_control = MechalinoControl()
 
-print("Press q to quit\nf to move forward\nb to move backward\nl to rotate left\nr to rotate right")
-
 while(True):
-    a = msvcrt.getch().decode('utf-8')
-    if a == 'q':
-        break
-    elif a == 'f':
-        mechalino_control.forward(3)
-    elif a == 'b': 
-        mechalino_control.backward(3)
-    elif a == 'l':
-        mechalino_control.rotate_ccw(90)
-    elif a == 'r':
-        mechalino_control.rotate_cw(90)
+    command = input('Enter commend (e.g. "F 1000"): ')
+    if command.startswith('F'):
+        mechalino_control.forward(int(command.replace('F ',''))/1000)
+    if command.startswith('B'):
+        mechalino_control.backward(int(command.replace('B ',''))/1000)
+    if command.startswith('R'):
+        mechalino_control.rotate_cw(int(command.replace('R ',''))/1000)
+    if command.startswith('L'):
+        mechalino_control.rotate_ccw(int(command.replace('L ',''))/1000)
+# print("Press q to quit\nf to move forward\nb to move backward\nl to rotate left\nr to rotate right")
+
+# while(True):
+#     a = msvcrt.getch().decode('utf-8')
+#     if a == 'q':
+#         break
+#     elif a == 'f':
+#         mechalino_control.forward(3)
+#     elif a == 'b': 
+#         mechalino_control.backward(3)
+#     elif a == 'l':
+#         mechalino_control.rotate_ccw(90)
+#     elif a == 'r':
+#         mechalino_control.rotate_cw(90)
 
